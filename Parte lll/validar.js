ffe4fe2bc862449e72233fdda0c188c1b2e1e3d6
Karen Alejandra Velasco Guanoluisa <64505621+kavelasco1@users.noreply.github.com>
2021-11-23 22:39:33 -0500

    Tarea 03

---
+++ Parte lll/validar.js
@@ -0,0 +1,34 @@
+function validar() {
+	// body...
+	var correo, telefono, expresion;
+	
+	correo= document.getElementById('txtEmail').value;
+	telefono= document.getElementById('txtTelefono').value;
+	expresion= /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
+	
+	if(!expresion.test(correo)){
+		alert("El correo no es válido");
+		return false;
+	}
+	
+	else if(telefono.length != 10){
+		alert("El telefono es INCORRECTO");
+		return false;
+	}
+	else if(isNaN(telefono)){
+		alert("El telefono ingresado no es un número");
+		return false;
+	}
+	
+}
+function checkSubmit() {
+    document.getElementById("btnEnviar").value = "Enviando...";
+    document.getElementById("btnEnviar").disabled = true;
+    return true;
+}
+
+function redirect(){
+alert("DATOS ENVIADOS CORRECTAMENTE !!!!!");
+var frm=document.getElementById("main-form");
+frm.action="../index.html"
+}
